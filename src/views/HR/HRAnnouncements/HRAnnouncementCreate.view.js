d22484aa01fea870b31d3e97299412c0472826ec
unknown <aashish.electrovese>
2023-03-02 19:15:21 +0530

    Chnages

--- src/views/HR/HRAnnouncements/HRAnnouncementCreate.view.js
+++ src/views/HR/HRAnnouncements/HRAnnouncementCreate.view.js
@@ -113,7 +113,6 @@ const HRAnnouncementCreateView = ({}) => {
             fullWidth={true}
             name="document"
             label=""
-            type={["jpg", "png", "pdf"]}
             default_image={form?.document ? form?.document : null}
             // user_image={form?.image}
             error={errorData?.image}
