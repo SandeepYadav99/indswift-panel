2711284f1842d100ce61e0f68e30270c9a1584d3
Himanshu <r.himanshuelectrovese@gmail.com>
2023-05-08 16:21:24 +0530

    lower btn added in pms

--- src/views/Pms/PmsReview/components/BottomAction/BottomAction.view.js
+++ src/views/Pms/PmsReview/components/BottomAction/BottomAction.view.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React from "react";
+import styles from "./Style.module.css";
+import { ButtonBase } from "@material-ui/core";
 
 const BottomActionView = () => {
-    return (
-        <div>
-            ds
+  return (
+    <div className={styles.stickyBtnWrap}>
+      <div className={styles.WrapOuter}>
+        <div className={styles.upperWrap}>
+          <span>
+            REVIEWERS: <span className={styles.upperNum}>32</span>
+          </span>
+          <span>
+            EMPLOYEES: <span className={styles.upperNum}>32</span>
+          </span>
         </div>
-    )
+        <div className={styles.btnWrap}>
+          <ButtonBase
+            aria-haspopup="true"
+            //   onClick={}
+            className={"createBtn"}
+          >
+            SEND REVIEW FORM
+          </ButtonBase>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default BottomActionView
+export default BottomActionView;
