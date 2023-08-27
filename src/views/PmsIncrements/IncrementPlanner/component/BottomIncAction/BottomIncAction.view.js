92464ff7ccbff7d32efa9d6118a2838f2bf631ad
charanjeet singh <charanjit.singh25@gmail.com>
2023-08-27 11:15:49 +0530

    salary field mobile view updated
    (cherry picked from commit 92a741ab11e38226536d77ca916808b50fac7ac0)

--- src/views/PmsIncrements/IncrementPlanner/component/BottomIncAction/BottomIncAction.view.js
+++ src/views/PmsIncrements/IncrementPlanner/component/BottomIncAction/BottomIncAction.view.js
@@ -3,31 +3,41 @@ import styles from "./Style.module.css";
 import { ButtonBase } from "@material-ui/core";
 
 const BottomIncActionView = ({
-  isSubmitting,
-  handleSend,
-}) => {
+                               isSubmitting,
+                               handleSend,
+                               handleFreeze
+                             }) => {
   return (
-    <div className={styles.stickyBtnWrap}>
-      <div className={styles.WrapOuter}>
-        <div className={styles.upperWrap}>
+      <div className={styles.stickyBtnWrap}>
+        <div className={styles.WrapOuter}>
+          <div className={styles.upperWrap}>
           <span>
             NOTE: IF A USER DOES NOT QUALIFY IN RANGE, FIRST RANGE INCREMENT
             WILL BE APPLIED.
           </span>
-          <span>Slab 1 will be applied on grade if a grade is not mapped.</span>
-        </div>
-        <div className={styles.btnWrap}>
-          <ButtonBase
-            disabled={isSubmitting}
-            aria-haspopup="true"
-            onClick={handleSend}
-            className={"createBtn"}
-          >
-            Save Increment
-          </ButtonBase>
+            <span>Slab 1 will be applied on grade if a grade is not mapped.</span>
+          </div>
+          <div className={styles.btnWrap}>
+            <ButtonBase
+                disabled={isSubmitting}
+                aria-haspopup="true"
+                onClick={handleSend}
+                className={"createBtn"}
+            >
+              Save Increment
+            </ButtonBase>
+            &nbsp;&nbsp;
+            <ButtonBase
+                disabled={isSubmitting}
+                aria-haspopup="true"
+                onClick={handleFreeze}
+                className={"createBtn"}
+            >
+              Freeze
+            </ButtonBase>
+          </div>
         </div>
       </div>
-    </div>
   );
 };
 
