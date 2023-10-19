f0ed1a905bc581f358d0b1c128367d3de42fa650
sandeep.electrovese <sandeepelectrovese@gmail.com>
2023-10-19 18:30:46 +0530

    Expiriing Offer Letter

--- src/views/ExpiringOfferLetter/ExpiringOfferLetter_View.js
+++ src/views/ExpiringOfferLetter/ExpiringOfferLetter_View.js
@@ -1,9 +1,282 @@
-import React from 'react'
+import React, { Component, useCallback, useEffect, useMemo } from "react";
+import {
+  Button,
+  Paper,
+  Checkbox,
+  IconButton,
+  MenuItem,
+  ButtonBase,
+} from "@material-ui/core";
+import classNames from "classnames";
+import { connect, useSelector } from "react-redux";
+import {
+  Add,
+  AssignmentOutlined,
+  InfoOutlined,
+  OpenInNew,
+  PeopleOutlined,
+} from "@material-ui/icons";
+import PageBox from "../../components/PageBox/PageBox.component";
+import styles from "./Style.module.css";
+import DataTables from "../../Datatables/Datatable.table";
+import Constants from "../../config/constants";
+import FilterComponent from "../../components/Filter/Filter.component";
+import StatusPill from "../../components/Status/StatusPill.component";
+import historyUtils from "../../libs/history.utils";
+import RouteName from "../../routes/Route.name";
+import useExpiringOfferLetterHook from "./ExpiringOfferLetter_hook";
 
-const ExpiringOfferLetterView = () => {
+const ExpiringOfferLetterView = ({ location }) => {
+  const {
+    handleSortOrderChange,
+    handleRowSize,
+    handlePageChange,
+    handleDataSave,
+    handleDelete,
+    handleEdit,
+    handleFilterDataChange,
+    handleSearchValueChange,
+    handleSideToggle,
+    handleViewDetails,
+    editData,
+    isSidePanel,
+    changeRoute,
+    changeEmployeeRoute,
+    isCalling,
+    configFilter,
+    warehouses,
+  } = useExpiringOfferLetterHook({});
+
+  const {
+    data,
+    all: allData,
+    currentPage,
+    is_fetching: isFetching,
+  } = useSelector((state) => state.review_olr);
+
+  const removeUnderScore = (value) => {
+    return value ? value.replace(/_/, " ") : "";
+  };
+  const renderStatus = useCallback((status) => {
+    return <StatusPill status={status} />;
+  }, []);
+
+  const renderFirstCell = useCallback((obj) => {
+    if (obj) {
+      return (
+        <div className={styles.firstCellFlex}>
+          <div className={classNames(styles.firstCellInfo, "openSans")}>
+            <span className={styles.productName}>{obj?.candidate?.name}</span>{" "}
+            <br />
+          </div>
+        </div>
+      );
+    }
+    return null;
+  }, []);
+  const renderPRCCell = useCallback((obj) => {
+    if (obj) {
+      return (
+        <div className={styles.hyperlinkText} onClick={() => changeRoute(obj)}>
+          <div>{obj?.job_details?.code}</div>
+        </div>
+      );
+    }
+    return null;
+  }, []);
+  const renderReplacingCell = useCallback((obj) => {
+    if (obj) {
+      return (
+        <div>
+            {
+                obj?.replacing_person?.name ? <div
+                className={styles.hyperlinkText}
+                onClick={() => changeEmployeeRoute(obj?.replacing_person)}
+              >
+                {obj?.replacing_person?.name}
+              </div> : <div>N/A</div>
+            }
+
+          <br />
+          {obj?.replacing_person?.code}
+        </div>
+      );
+    }
+    return null;
+  }, []);
+
+  const tableStructure = useMemo(() => {
+    return [
+      {
+        key: "candidate_name",
+        label: "CANDIDATE NAME",
+        sortable: true,
+        render: (value, all) => <div>{renderFirstCell(all)}</div>,
+      },
+      {
+        key: "code",
+        label: "PRC",
+        sortable: false,
+        render: (temp, all) => <div>{renderPRCCell(all)}</div>,
+      },
+      {
+        key: "department",
+        label: "DESIGNATION/ DEPARTMENT",
+        sortable: false,
+        render: (temp, all) => (
+          <div>
+            {all?.designation?.name ? all?.designation?.name : all?.job_details?.designation}/{all?.job_details?.department}
+          </div>
+        ),
+      },
+      {
+        key: "replacing_person",
+        label: "REPLACING PERSON",
+        sortable: false,
+        render: (temp, all) => <div>{renderReplacingCell(all)}</div>,
+      },
+      {
+        key: "olr",
+        label: "OLR",
+        sortable: false,
+        render: (temp, all) => <div>{all?.olr}</div>,
+      },
+      {
+        key: "offer_letter_status",
+        label: "OFFER LETTER STATUS",
+        sortable: false,
+        render: (temp, all) => (
+          <div>
+            <StatusPill status={all?.status} />
+          </div>
+        ),
+      },
+      {
+        key: "candidateObj.status",
+        label: "Candidate STATUS",
+        sortable: true,
+        render: (temp, all) => (
+            <div>
+              <StatusPill status={removeUnderScore(all?.candidate?.status)} />
+            </div>
+        ),
+      },
+      {
+        key: "offerObj.joining_date",
+        label: "Joining Date",
+        sortable: true,
+        render: (temp, all) => (
+            <div>
+              {all?.offer?.joining_date}
+            </div>
+        ),
+      },
+      {
+        key: "expiring_on",
+        label: "EXPIRING ON",
+        sortable: false,
+        render: (temp, all) => <div>{all?.createdAtText}</div>,
+      },
+      {
+        key: "user_id",
+        label: "Action",
+        render: (temp, all) => (
+          <div className={styles.btnWrapContainer}>
+            <IconButton
+              className={"tableActionBtn"}
+              color="secondary"
+              disabled={isCalling}
+              onClick={() => {
+                handleViewDetails(all);
+              }}
+            >
+              <PeopleOutlined fontSize={"small"} className={styles.openIcon} />{" "}
+              <span className={styles.subText}>Expire Offer</span>
+            </IconButton>
+            {all.status === Constants.OFFER_LETTER_STATUS.PENDING && (
+              <IconButton
+                className={"tableActionBtn"}
+                color="secondary"
+                disabled={isCalling}
+                onClick={() => {
+                  historyUtils.push(
+                    `${RouteName.CANDIDATES_OFFER_DETAILS}${all?.offer_id}`,
+                    {
+                      isReview: true,
+                      reviewId: all?.id,
+                    }
+                  );
+                }}
+              >
+                <AssignmentOutlined
+                  fontSize={"small"}
+                  className={styles.openIcon}
+                />{" "}
+                <span className={styles.subText}>Resend Offer</span>
+              </IconButton>
+            )}
+          </div>
+        ),
+      },
+    ];
+  }, [renderStatus, renderFirstCell, handleViewDetails, handleEdit, isCalling]);
+
+  const tableData = useMemo(() => {
+    const datatableFunctions = {
+      onSortOrderChange: handleSortOrderChange,
+      onPageChange: handlePageChange,
+      onRowSizeChange: handleRowSize,
+    };
+
+    const datatable = {
+      ...Constants.DATATABLE_PROPERTIES,
+      columns: tableStructure,
+      data: data,
+      count: allData.length,
+      page: currentPage,
+    };
+
+    return { datatableFunctions, datatable };
+  }, [
+    allData,
+    tableStructure,
+    handleSortOrderChange,
+    handlePageChange,
+    handleRowSize,
+    data,
+    currentPage,
+  ]);
+
   return (
-    <div>Expiring Offer Letter</div>
-  )
-}
+    <div>
+      <PageBox>
+        <div className={styles.headerContainer}>
+          <div>
+            <span className={styles.title}>Offer Letters List</span>
+            <div className={styles.newLine} />
+          </div>
+        </div>
 
-export default ExpiringOfferLetterView
+        <div>
+          <FilterComponent
+            is_progress={isFetching}
+            filters={configFilter}
+            handleSearchValueChange={handleSearchValueChange}
+            handleFilterDataChange={handleFilterDataChange}
+          />
+          <div>
+            <br />
+            <div style={{ width: "100%" }}>
+              <DataTables
+                {...tableData.datatable}
+                {...tableData.datatableFunctions}
+              />
+            </div>
+          </div>
+        </div>
+      </PageBox>
+    </div>
+  );
+};
+
+export default ExpiringOfferLetterView;
---
+++ src/views/ExpiringOfferLetter/ExpiringOfferLetter_hook.js
@@ -0,0 +1,129 @@
+import {useDispatch, useSelector} from "react-redux";
+import {useCallback, useEffect, useMemo, useRef, useState} from "react";
+import {
+    actionCreateReviewOLR, actionDeleteReviewOLR,
+    actionFetchReviewOLR,
+    actionSetPageReviewOLR,
+    actionUpdateReviewOLR
+} from "../../actions/ReviewOLR.action";
+import LogUtils from "../../libs/LogUtils";
+import RouteName from "../../routes/Route.name";
+import historyUtils from "../../libs/history.utils";
+import Constants from "../../config/constants";
+
+
+const useExpiringOfferLetterHook = ({}) => {
+    const [isSidePanel, setSidePanel] = useState(false);
+    const [isCalling, setIsCalling] = useState(false);
+    const [editData, setEditData] = useState(null);
+    const dispatch = useDispatch();
+    const isMountRef = useRef(false);
+    const {sorting_data: sortingData, is_fetching: isFetching, query, query_data: queryData} = useSelector(state => state.review_olr);
+
+    useEffect(() => {
+        dispatch(actionFetchReviewOLR(1, sortingData, {
+            query: isMountRef.current ? query : null,
+            query_data: isMountRef.current ? queryData : null,
+
+        }));
+        isMountRef.current = true;
+    }, []);
+
+    const changeRoute = useCallback((data) => {
+        historyUtils.push(RouteName.JOB_OPENINGS_DETAILS+data?.job_details?.id) //+data.id
+    }, []);
+    const changeEmployeeRoute = useCallback((data) => {
+        historyUtils.push(`/employees/details/${data?.code}`);
+    }, []);
+    const handlePageChange = useCallback((type) => {
+        dispatch(actionSetPageReviewOLR(type));
+    }, []);
+
+    const handleDataSave = useCallback((data, type) =>  {
+        if (type == 'CREATE') {
+            dispatch(actionCreateReviewOLR(data));
+        } else {
+            dispatch(actionUpdateReviewOLR(data));
+        }
+        setSidePanel(e => !e);
+        setEditData(null);
+    }, [setSidePanel, setEditData]);
+
+    const queryFilter = useCallback((key, value) => {
+        dispatch(actionFetchReviewOLR(1, sortingData, {
+            query: key == 'SEARCH_TEXT' ? value : query,
+            query_data: key == 'FILTER_DATA' ? value : queryData,
+        }));
+        // dispatch(actionFetchReviewOLR(1, sortingData))
+    }, [sortingData, query, queryData,]);
+
+    const handleFilterDataChange = useCallback((value) => {
+        queryFilter('FILTER_DATA', value);
+    }, [queryFilter]);
+
+    const handleSearchValueChange = useCallback((value) => {
+        queryFilter('SEARCH_TEXT', value);
+    }, [queryFilter]);
+
+
+
+    const handleSortOrderChange = useCallback((row, order) => {
+        // dispatch(actionSetPageReviewOLR(1));
+        dispatch(actionFetchReviewOLR(1, {row, order}, {
+            query: query,
+            query_data: queryData,
+        }))
+    }, [query, queryData]);
+
+    const handleRowSize = (page) => {
+        console.log(page);
+    }
+
+    const handleDelete = useCallback((id) => {
+        dispatch(actionDeleteReviewOLR(id));
+        setSidePanel(false);
+        setEditData(null);
+    }, [setEditData, setSidePanel]);
+
+    const handleEdit = useCallback((data) => {
+        setEditData(data);
+        setSidePanel(e => !e);
+    }, [setEditData, setSidePanel]);
+
+    const handleSideToggle = useCallback(() => {
+        historyUtils.push(RouteName.CANDIDATES_CREATE)
+    }, [setEditData, setSidePanel]);
+
+    const handleViewDetails = useCallback((data) => {
+        LogUtils.log('data', data);
+        historyUtils.push(`${RouteName.CANDIDATES_DETAILS}${data.candidate_id}`)
+    }, []);
+    const configFilter = useMemo(() => {
+        return [
+            {label: 'Created Date', options: { maxDate: new Date() },  name: 'createdAt', type: 'date'},
+            {label: 'Status', name: 'status', type: 'select', fields: ['APPROVED', 'PENDING','REJECTED']},
+            {label: 'Candidate Status', name: 'candidateObj.status', type: 'select', fields: Object.keys(Constants.JOB_CANDIDATE_STATUS)},
+        ];
+    }, []);
+
+    return {
+        handlePageChange,
+        handleDataSave,
+        handleFilterDataChange,
+        handleSearchValueChange,
+        handleRowSize,
+        handleSortOrderChange,
+        handleDelete,
+        handleEdit,
+        handleSideToggle,
+        handleViewDetails,
+        isCalling,
+        editData,
+        isSidePanel,
+        configFilter,
+        changeRoute,
+        changeEmployeeRoute
+    }
+};
+
+export default useExpiringOfferLetterHook;
