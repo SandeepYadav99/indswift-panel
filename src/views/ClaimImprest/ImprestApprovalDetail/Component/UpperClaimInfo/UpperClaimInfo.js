eccabaac400e6ce49e2ca46da75bef6e4978e37d
Deepesh Giri <deepeshelectrovese@gmail.com>
2024-01-08 15:02:25 +0530

    Done Changes Imprest Module

--- src/views/ClaimImprest/ImprestApprovalDetail/Component/UpperClaimInfo/UpperClaimInfo.js
+++ src/views/ClaimImprest/ImprestApprovalDetail/Component/UpperClaimInfo/UpperClaimInfo.js
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./Style.module.css";
 import image from "../../../../../assets/img/download.png";
 
-function UpperClaimInfo({ data,isLoc }) {
+function UpperClaimInfo({ data, isLoc, isLoan  }) {
   return (
     <div className={styles.plainPaper}>
       <div className={styles.newContainer}>
@@ -12,13 +12,13 @@ function UpperClaimInfo({ data,isLoc }) {
 
         <div className={styles.mainFlex}>
           <div className={styles.left221}>
-            <div>
+            <div className={styles.imageContainer}>
               <img
                 className={styles.claimimg}
                 src={data?.image ? data?.image : image}
               />
             </div>
-            <div>
+            <div className={styles.nameData}>
               <div className={styles.key}>
                 <span className={styles.value}>Name:</span>
                 {data?.employee?.name}
@@ -31,26 +31,58 @@ function UpperClaimInfo({ data,isLoc }) {
                 <span className={styles.value}>Location:</span>
                 {data?.employee?.location?.name}
               </div>
-             { isLoc && <div className={styles.key}>
-                <span className={styles.value}>Date of Joining:</span>
-                {data?.employee?.doj}
-              </div>}
+              {(isLoc || isLoan) && (
+                <div className={styles.key}>
+                  <span className={styles.value}>Date of Joining:</span>
+                  {data?.employee?.dojText}
+                </div>
+              )}
             </div>
           </div>
           <div className={styles.vertical}></div>
           <div className={styles.left}>
-            <div className={styles.key}>
-              <span className={styles.value}>Designation:</span>
-              {data?.employee?.designation?.name}
+            <div className={styles.nameHigher}>
+              <div className={styles.key}>
+                <span className={styles.value}>Name:</span>
+                <span>{data?.employee?.name}</span>
+              </div>
+              <div className={styles.key}>
+                <span className={styles.value}>Employee ID:</span>
+                <span>{data?.employee?.emp_code}</span>
+              </div>
+              <div className={styles.key}>
+                <span className={styles.value}>Location:</span>
+                <span>{data?.employee?.location?.name}</span>
+              </div>
+              {(isLoc || isLoan) && (
+                <div className={styles.key}>
+                  <span className={styles.value}>Date of Joining:</span>
+                  <span>{data?.employee?.dojText}</span>
+                </div>
+              )}
             </div>
-            <div className={styles.key}>
-              <span className={styles.value}>Grade/Cadre:</span>
-              {data?.employee?.grade?.code &&
-                `${data?.employee?.grade?.code} / ${data?.employee?.cadre?.code}`}
-            </div>
-            <div className={styles.key}>
-              <span className={styles.value}>Department:</span>
-              {data?.employee?.department?.name}
+            <div className={styles.verticalLine}></div>
+            <div className={styles.leftPartMarginMobile}>
+              <div className={styles.key}>
+                <span className={styles.value}>Designation:</span>
+                <span>{data?.employee?.designation?.name}</span>
+              </div>
+              <div className={styles.key}>
+                <span className={styles.value}>Grade/Cadre:</span>
+                <span>{`${data?.employee?.grade?.code} / ${data?.employee?.cadre?.code}`}</span>
+              </div>
+              <div className={styles.key}>
+                <span className={styles.value}>Department:</span>
+                <span>{data?.employee?.department?.name}</span>
+              </div>
+              {isLoan && (
+                <div className={styles.key}>
+                  <span className={styles.value}>
+                    Experience with Organization:
+                  </span>
+                  <span> {data?.employee?.experience?.current}</span>
+                </div>
+              )}
             </div>
           </div>
         </div>
