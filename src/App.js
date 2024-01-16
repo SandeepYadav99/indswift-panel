184eb4f53cceaaec55166a17fac52003ed40fbd6
charanjeet singh <charanjit.singh25@gmail.com>
2024-01-16 16:02:24 +0530

    single card

--- src/App.js
+++ src/App.js
@@ -1,43 +1,53 @@
-import React, {Component} from 'react';
+import React, {useMemo} from 'react';
 import {Router, Route, Switch} from "react-router-dom";
 import {MuiThemeProvider, createTheme} from '@material-ui/core/styles';
 import RouteComponent from './routes/index.route';
 import './App.css';
 import themes, {overrides} from './themes';
 import history from './libs/history.utils';
-import {connect} from "react-redux";
+import {connect, useSelector} from "react-redux";
 import {bindActionCreators} from "redux";
+import {isIosSafari} from "./libs/general.utils";
 
 // const history = createBrowserHistory();
 
 
 
-class App extends Component {
-    render() {
-        const { themeType } = this.props;
-        const themeDefault = themeType == 'dark' ? themes.dark : themes.default;
+const App = ({}) => {
+    const { theme: themeType } = useSelector(state => state.app_setting);
+    const themeDefault = themeType == 'dark' ? themes.dark : themes.default;
+
 // themeDefault['palette']['type'] = 'dark';
-        const theme = createTheme({...themeDefault, ...overrides});
-        return (
-            <MuiThemeProvider theme={theme}>
-                <Router history={history}>
-                    <RouteComponent/>
-                </Router>
-            </MuiThemeProvider>
-        );
-    }
+    const theme = createTheme({...themeDefault, ...overrides});
+
+    const iosAddButton = useMemo(() => {
+        const iOSIsInstalled = window?.navigator?.standalone;
+        const isIos = isIosSafari();
+        if (isIos && !iOSIsInstalled) {
+            return (<div className={'installDiv'} style={{display: 'flex'}}>
+                <div>
+                    Add To Home Screen
+                </div>
+                <button>Add +</button>
+            </div>);
+        }
+    }, []);
+
+    return (
+        <MuiThemeProvider theme={theme}>
+            <Router history={history}>
+                <RouteComponent/>
+            </Router>
+            <div id={'installPopUp'} className={'installDiv'}>
+                <div>
+                    Add To Home Screen
+                </div>
+                <button id="install">Add +</button>
+
+            </div>
+            {iosAddButton}
+        </MuiThemeProvider>
+    );
 }
-function mapDispatchToProps(dispatch) {
-    return bindActionCreators({
 
-    })
-}
-
-function mapStateToProps(state) {
-    return {
-        themeType: state.app_setting.theme,
-    }
-}
-
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default (App);
