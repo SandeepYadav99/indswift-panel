4ddf0b464a2db6057cad28427ed08ddb70a8fac5
Deepesh Giri <deepeshelectrovese@gmail.com>
2024-01-30 15:09:20 +0530

    done with changes

--- src/views/MobileLandingPage/MobileLandingPage.component.js
+++ src/views/MobileLandingPage/MobileLandingPage.component.js
@@ -2,17 +2,18 @@ import styles from "./styled.module.css";
 import { useEffect } from "react";
 import history from "../../libs/history.utils";
 import { Link } from "react-router-dom";
-import Attendance from "../../assets/Mobile/ic_attendance.png";
-import knowledgeCenter from "../../assets/Mobile/ic_knowledge.png";
-import LeaveApplication from "../../assets/Mobile/ic_application.png";
-import Circular from "../../assets/Mobile/ic_circular.png";
-import Engagement from "../../assets/Mobile/ic_engage.png";
-import HCMModule from "../../assets/Mobile/ic_hcm.png";
+import Attendance from "../../assets/Mobile/ic_attendance@2x.png";
+import knowledgeCenter from "../../assets/Mobile/ic_knowledge@2x.png";
+import LeaveApplication from "../../assets/Mobile/ic_application@2x.png";
+import Circular from "../../assets/Mobile/ic_circular@2x.png";
+import Engagement from "../../assets/Mobile/ic_engage@2x.png";
+import HCMModule from "../../assets/Mobile/ic_hcm@2x (1).png";
 import IndiswiftBannerImage from "../../assets/Mobile/SkyNetBannerMobile.jpg";
 import Example from "./Lotties.component";
+/*Looties Imported here*/
 import claimsData from "../../assets/Lotties/claims.json";
 import travellottie from "../../assets/Lotties/travel.json";
-import profilelottie from "../../assets/Lotties/profile.json";
+import profilelottie from "../../assets/Lotties/profile_new.json";
 import loanlottie from "../../assets/Lotties/loan.json";
 import imprestlottie from "../../assets/Lotties/imprest.json";
 import policylotties from "../../assets/Lotties/policy.json";
@@ -37,7 +38,7 @@ const MobileLandingPage = () => {
           <div className={styles.plainPaper}>
             <Link to={"/my/profile"} className={styles.linkRoutes}>
               <div className={styles.subPaper}>
-                <Example animation={profilelottie} />
+                <Example animation={profilelottie}  style={{ height: "60px", width: "60px" }}/>
                 <div className={styles.text}> My Profile</div>
               </div>
             </Link>
@@ -46,7 +47,7 @@ const MobileLandingPage = () => {
             <Link to={"/employee/dashboard"} className={styles.linkRoutes}>
               <div className={styles.subPaper}>
                 <div className={styles.imgBox}>
-                  <img alt="text" src={Engagement} />
+                  <img alt="text" src={Engagement}  style={{ height: "60px", width: "60px" }}/>
                 </div>
                 <div className={styles.text}>Engage Dashboard</div>
               </div>
@@ -56,7 +57,11 @@ const MobileLandingPage = () => {
             <Link to={"/leave-application/list"} className={styles.linkRoutes}>
               <div className={styles.subPaper}>
                 <div className={styles.imgBox}>
-                  <img alt="text" src={LeaveApplication} />
+                  <img
+                    alt="text"
+                    src={LeaveApplication}
+                    style={{ height: "60px", width: "60px" }}
+                  />
                 </div>
                 <div className={styles.text}>My leave Application</div>
               </div>
@@ -68,7 +73,11 @@ const MobileLandingPage = () => {
             <Link to={"/employee/engagement"} className={styles.linkRoutes}>
               <div className={styles.subPaper}>
                 <div className={styles.imgBox}>
-                  <img alt="text" src={HCMModule} />
+                  <img
+                    alt="text"
+                    src={HCMModule}
+                    style={{ height: "60px", width: "60px" }}
+                  />
                 </div>
                 <div className={styles.text}>HCM Module</div>
               </div>
@@ -78,7 +87,11 @@ const MobileLandingPage = () => {
             <Link to={"/employeecircular"} className={styles.linkRoutes}>
               <div className={styles.subPaper}>
                 <div className={styles.imgBox}>
-                  <img alt="text" src={Circular} />
+                  <img
+                    alt="text"
+                    src={Circular}
+                    style={{ height: "50px", width: "50px" }}
+                  />
                 </div>
                 <div className={styles.text}>Circulars</div>
               </div>
@@ -107,7 +120,11 @@ const MobileLandingPage = () => {
             <Link to={"/employee/knowledge"} className={styles.linkRoutes}>
               <div className={styles.subPaper}>
                 <div className={styles.imgBox}>
-                  <img alt="text" src={knowledgeCenter} />
+                  <img
+                    alt="text"
+                    src={knowledgeCenter}
+                    style={{ height: "60px", width: "60px" }}
+                  />
                 </div>
                 <div className={styles.text}>Knowledge Centre</div>
               </div>
@@ -116,7 +133,7 @@ const MobileLandingPage = () => {
           <div className={styles.plainPaper}>
             <Link to={"/employees/claim/imprest"} className={styles.linkRoutes}>
               <div className={styles.subPaper}>
-              <Example animation={imprestlottie} />
+                <Example animation={imprestlottie} />
 
                 <div className={styles.text}>Imprest</div>
               </div>
@@ -127,7 +144,7 @@ const MobileLandingPage = () => {
           <div className={styles.plainPaper}>
             <Link to={"/travel/plan"} className={styles.linkRoutes}>
               <div className={styles.subPaper}>
-              <Example animation={travellottie} />
+                <Example animation={travellottie} />
 
                 <div className={styles.text}>Travel Planner</div>
               </div>
@@ -151,136 +168,6 @@ const MobileLandingPage = () => {
             <Link to={"/loan/list"} className={styles.linkRoutes}>
               <div className={styles.subPaper}>
                 <Example animation={loanlottie} />
-                <div className={styles.text}>Loan</div>
-              </div>
-            </Link>
-          </div>
-        </div>
-      </div>
-      <div className={styles.mobileTwo}>
-        <div className={styles.subContainer}>
-          <div className={styles.plainPaper}>
-            <Link to={"/my/profile"} className={styles.linkRoutes}>
-              <div className={styles.subPaper}>
-                <Example animation={profilelottie} />
-                <div className={styles.text}> My Profile</div>
-              </div>
-            </Link>
-          </div>
-          <div className={styles.plainPaper}>
-            <Link to={"/employee/dashboard"} className={styles.linkRoutes}>
-              <div className={styles.subPaper}>
-                <div className={styles.imgBox}>
-                  <img alt="text" src={Engagement} />
-                </div>
-                <div className={styles.text}>Engage Dashboard</div>
-              </div>
-            </Link>
-          </div>
-        </div>
-        <div className={styles.subContainer}>
-          <div className={styles.plainPaper}>
-            <Link to={"/leave-application/list"} className={styles.linkRoutes}>
-              <div className={styles.subPaper}>
-                <div className={styles.imgBox}>
-                  <img alt="text" src={LeaveApplication} />
-                </div>
-                <div className={styles.text}>My leave Application</div>
-              </div>
-            </Link>
-          </div>
-          <div className={styles.plainPaper}>
-            <Link to={"/cm/claims"} className={styles.linkRoutes}>
-              <div className={styles.subPaper}>
-                <Example animation={claimsData} />
-                <div className={styles.text}>Claims</div>
-              </div>
-            </Link>
-          </div>
-        </div>
-        <div className={styles.subContainer}>
-          <div className={styles.plainPaper}>
-            <Link to={"/employee/engagement"} className={styles.linkRoutes}>
-              <div className={styles.subPaper}>
-                <div className={styles.imgBox}>
-                  <img alt="text" src={HCMModule} />
-                </div>
-                <div className={styles.text}>HCM Module</div>
-              </div>
-            </Link>
-          </div>
-          <div className={styles.plainPaper}>
-            <Link to={"/employeecircular"} className={styles.linkRoutes}>
-              <div className={styles.subPaper}>
-                <div className={styles.imgBox}>
-                  <img alt="text" src={Circular} />
-                </div>
-                <div className={styles.text}>Circulars</div>
-              </div>
-            </Link>
-          </div>
-        </div>
-        <div className={styles.subContainer}>
-          <div className={styles.plainPaper}>
-            <Link to={"/hrpolicy"} className={styles.linkRoutes}>
-              <div className={styles.subPaper}>
-                <Example animation={policylotties} />
-
-                <div className={styles.text}>HR policies</div>
-              </div>
-            </Link>
-          </div>
-          <div className={styles.plainPaper}>
-            <Link to={"/employee/knowledge"} className={styles.linkRoutes}>
-              <div className={styles.subPaper}>
-                <div className={styles.imgBox}>
-                  <img alt="text" src={knowledgeCenter} />
-                </div>
-                <div className={styles.text}>Knowledge Centre</div>
-              </div>
-            </Link>
-          </div>
-        </div>
-        <div className={styles.subContainer}>
-          <div className={styles.plainPaper}>
-            <Link to={"/employees/claim/imprest"} className={styles.linkRoutes}>
-              <div className={styles.subPaper}>
-              <Example animation={imprestlottie} />
-
-                <div className={styles.text}>Imprest</div>
-              </div>
-            </Link>
-          </div>
-          <div className={styles.plainPaper}>
-            <Link to={"/travel/plan"} className={styles.linkRoutes}>
-              <div className={styles.subPaper}>
-              <Example animation={travellottie} />
-
-                <div className={styles.text}>Travel Planner</div>
-              </div>
-            </Link>
-          </div>
-        </div>
-        <div className={styles.subContainer}>
-          <div className={styles.plainPaper}>
-            <a
-              href={"http://122.186.44.85/TOS7x1/frmLogin.aspx"}
-              className={styles.linkRoutes}
-              target="_blank"
-            >
-              <div className={styles.subPaper}>
-                <div className={styles.imgBox}>
-                  <img alt="text" src={Attendance} style={{ height: "40px" }} />
-                </div>
-                <div className={styles.text}>Attendance</div>
-              </div>
-            </a>
-          </div>
-          <div className={styles.plainPaper}>
-            <Link to={"/loan/list"} className={styles.linkRoutes}>
-              <div className={styles.subPaper}>
-                <Example animation={loanlottie} />
-
                 <div className={styles.text}>Loan</div>
               </div>
             </Link>
