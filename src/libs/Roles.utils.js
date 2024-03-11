5bd86b6acb2e2dc665001da4f2fd77819030f0b9
charanjeet singh <charanjit.singh25@gmail.com>
2024-03-11 15:49:23 +0530

    roles integration

--- src/libs/Roles.utils.js
+++ src/libs/Roles.utils.js
@@ -5,7 +5,6 @@ import LogUtils from "./LogUtils";
 class RolesUtils {
 
     canAccess = (accessRequiredPr, myRolePr = null) => {
-        LogUtils.log('canAccess', accessRequired, myRolePr);
         let myRole = null;
         const accessRequired = accessRequiredPr ? accessRequiredPr : [Constants.ROLES.GENERAL];
         if (!myRolePr) {
@@ -15,9 +14,11 @@ class RolesUtils {
         }
         if (typeof myRolePr === "string") {
             myRole = [myRolePr, Constants.ROLES.GENERAL];
-        } else {
+        } else if (Array.isArray(myRolePr)) {
             myRole = [...myRolePr, Constants.ROLES.GENERAL]
         }
+
+        LogUtils.log('canAccess', accessRequiredPr, myRolePr);
         const isThere = accessRequired.findIndex(role => myRole.indexOf(role)>= 0);
         return isThere >= 0;
     }
