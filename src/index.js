43cc5c8cdb6bb51d27f0a20b4377b8b5038d3942
himanshu-199 <karn07rawat@gmail.com>
2024-05-09 13:53:12 +0530

    sentry key implimented

--- src/index.js
+++ src/index.js
@@ -17,8 +17,25 @@ import SnackbarUtils from "./libs/SnackbarUtils";
 import {isIosSafari} from "./libs/general.utils";
 import historyUtils from "./libs/history.utils";
 import LogUtils from "./libs/LogUtils";
+import * as Sentry from "@sentry/react";
+import constants from './config/constants';
 
 setAxiosTimezone();
+Sentry.init({
+    dsn: constants.SENTRY_KEY,
+    integrations: [
+      Sentry.browserTracingIntegration(),
+      Sentry.replayIntegration(),
+    ],
+    // Performance Monitoring
+    tracesSampleRate: 1.0, //  Capture 100% of the transactions
+    // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+    tracePropagationTargets: [/^https:\/\/skynet\.indswiftlabs\.com/],
+    // Session Replay
+    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  });
+  
 if (localStorage.theme && false) {
     store.dispatch(actionChangeTheme(localStorage.theme));
 } else {
