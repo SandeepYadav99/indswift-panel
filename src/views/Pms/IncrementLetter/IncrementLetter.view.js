4c86f47d58997294cbe0e03a241d92c9f5b56e7f
himanshu-199 <karn07rawat@gmail.com>
2024-05-17 14:00:28 +0530

    year 2024 added

--- src/views/Pms/IncrementLetter/IncrementLetter.view.js
+++ src/views/Pms/IncrementLetter/IncrementLetter.view.js
@@ -512,6 +512,7 @@ const IncrementLetter = ({ location }) => {
         }}
       >
         <MenuItem value={"2023"}>2023</MenuItem>
+        <MenuItem value={"2024"}>2024</MenuItem>
       </CustomSelectField>
     );
   }, [year]);
