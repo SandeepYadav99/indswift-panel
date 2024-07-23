b3e6b99e091a5a6660dadfec0eb479a8334b3782
Sandeep Yadav <sandeepelectrovese@gmail.com>
2024-07-23 14:42:15 +0530

    FEATURES-> For swipe button  react-swipezor

--- src/views/UpdateAttendance/Component/SwipButton.js
+++ src/views/UpdateAttendance/Component/SwipButton.js
@@ -1,24 +1,23 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import styles from "./Style.module.css";
 import { ButtonBase } from "@material-ui/core";
 import rightSwip from "../../../assets/img/ic_right_swipe.png";
 import leftSwip from "../../../assets/img/ic_left_swipe.png";
-import successImage from "../../../assets/img/ic_punch_out_success@2x.png"
-
+import successImage from "../../../assets/img/ic_punch_out_success@2x.png";
+import SwipeButton from "react-swipezor";
 
 const SwipButton = ({ swipRightPunch, swipLeftPunch, isSwip, isLeftSwip }) => {
- 
   return (
     <div className={styles.buttonContainer}>
       {!isSwip || isLeftSwip ? (
-        <ButtonBase className={styles.swipBtn} onClick={swipRightPunch}>
-          SWIPE RIGHT TO PUNCH IN <img src={rightSwip} alt="" className={styles.swipImage} />
-        </ButtonBase>
-      
-       
+        // <ButtonBase className={styles.swipBtn} onClick={swipRightPunch}>
+        //   SWIPE RIGHT TO PUNCH IN <img src={rightSwip} alt="" className={styles.swipImage} />
+        // </ButtonBase>
+        <SwipeButton mainText="Swipe me" overlayText="S I K E" />
       ) : (
         <ButtonBase className={styles.swipLeftBtn} onClick={swipLeftPunch}>
-          SWIPE LEFT TO PUNCH OUT <img src={leftSwip} alt="" className={styles.swipImage}/>
+          SWIPE LEFT TO PUNCH OUT{" "}
+          <img src={leftSwip} alt="" className={styles.swipImage} />
         </ButtonBase>
       )}
     </div>
